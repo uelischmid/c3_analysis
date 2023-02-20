3eef5a074f3fd9baba4b5d2fef3549ad22d37d2c
Ueli Schmid <ueli.schmid@usys.ethz.ch>
2023-02-20 19:17:50 +0100

    betareg: change model building proces and add different formulae and links

--- code/analysis/02_betareg.R
+++ code/analysis/02_betareg.R
@@ -1,5 +1,5 @@
 ## analyize data with beta regression
-## 9.2.23, us
+## 20.2.23, us
 
 
 # setup -------------------------------------------------------------------
@@ -32,292 +32,113 @@ data_ST_LED <- data_analysis %>%
   filter(nat_haz == "LED")
 
 
-# build models for absolute values ----------------------------------------
-abs_model_info <- vector(mode = "list", length = 8)
 
-## LT A MP
-br_LT_A_MP_abs <- betareg(sha_i_MP_met_abs ~ stratum +
-                            q_site2 + q_reg2 +
-                            mgm_type +
-                            mgm_interval + mgm_intensity +
-                            I(mgm_interval^2) + I(mgm_intensity^2) +
-                            q_site2 * q_reg2 +
-                            mgm_interval * mgm_intensity,
-                          data = data_LT_A)
-abs_model_info[[1]] <- glance(br_LT_A_MP_abs) %>% 
-  mutate(model = "br_LT_A_MP_abs",
-         simtype = "LT",
-         nat_haz = "A",
-         profile = "MP")
+# prepare models ----------------------------------------------------------
+model_combinations <- expand_grid(simtype       = c("LT", "ST"),
+                                  nat_haz       = c("A", "LED"),
+                                  profile       = c("MP", "IP"),
+                                  resp_var_type = c("abs", "diff"),
+                                  formula_nr    = c(1:3),
+                                  link          = c("logit", "cloglog"))
 
-# LT A IP
-br_LT_A_IP_abs <- betareg(sha_i_IP_met_abs ~ stratum +
-                            q_site2 + q_reg2 +
-                            mgm_type +
-                            mgm_interval + mgm_intensity +
-                            I(mgm_interval^2) + I(mgm_intensity^2) +
-                            q_site2 * q_reg2 +
-                            mgm_interval * mgm_intensity,
-                          data = data_LT_A)
-abs_model_info[[2]] <- glance(br_LT_A_IP_abs) %>% 
-  mutate(model = "br_LT_A_IP_abs",
-         simtype = "LT",
-         nat_haz = "A",
-         profile = "IP")
-
-# LT LED MP
-br_LT_LED_MP_abs <- betareg(sha_i_MP_met_abs ~ stratum +
-                              q_site2 + q_reg2 +
-                              mgm_type +
-                              mgm_interval + mgm_intensity +
-                              I(mgm_interval^2) + I(mgm_intensity^2) +
-                              q_site2 * q_reg2 +
-                              mgm_interval * mgm_intensity,
-                            data = data_LT_LED)
-abs_model_info[[3]] <- glance(br_LT_LED_MP_abs) %>% 
-  mutate(model = "br_LT_LED_MP_abs",
-         simtype = "LT",
-         nat_haz = "LED",
-         profile = "MP")
-
-# LT LED IP
-br_LT_LED_IP_abs <- betareg(sha_i_IP_met_abs ~ stratum +
-                              q_site2 + q_reg2 +
-                              mgm_type +
-                              mgm_interval + mgm_intensity +
-                              I(mgm_interval^2) + I(mgm_intensity^2) +
-                              q_site2 * q_reg2 +
-                              mgm_interval * mgm_intensity,
-                            data = data_LT_LED)
-abs_model_info[[4]] <- glance(br_LT_LED_IP_abs) %>% 
-  mutate(model = "br_LT_LED_IP_abs",
-         simtype = "LT",
-         nat_haz = "LED",
-         profile = "IP")
-
-## ST A MP
-br_ST_A_MP_abs <- betareg(sha_i_MP_met_abs ~ stratum +
-                            q_site2 + q_reg2 +
-                            init +
-                            mgm_type +
-                            mgm_interval + mgm_intensity +
-                            I(mgm_interval^2) + I(mgm_intensity^2) +
-                            q_site2 * q_reg2 +
-                            mgm_interval * mgm_intensity,
-                          data = data_ST_A)
-abs_model_info[[5]] <- glance(br_ST_A_MP_abs) %>% 
-  mutate(model = "br_ST_A_MP_abs",
-         simtype = "ST",
-         nat_haz = "A",
-         profile = "MP")
-
-# ST A IP
-br_ST_A_IP_abs <- betareg(sha_i_IP_met_abs ~ stratum +
-                            q_site2 + q_reg2 +
-                            init +
-                            mgm_type +
-                            mgm_interval + mgm_intensity +
-                            I(mgm_interval^2) + I(mgm_intensity^2) +
-                            q_site2 * q_reg2 +
-                            mgm_interval * mgm_intensity,
-                          data = data_ST_A)
-abs_model_info[[6]] <- glance(br_ST_A_IP_abs) %>% 
-  mutate(model = "br_ST_A_IP_abs",
-         simtype = "ST",
-         nat_haz = "A",
-         profile = "IP")
-
-# ST LED MP
-br_ST_LED_MP_abs <- betareg(sha_i_MP_met_abs ~ stratum +
-                              q_site2 + q_reg2 +
-                              init +
-                              mgm_type +
-                              mgm_interval + mgm_intensity +
-                              I(mgm_interval^2) + I(mgm_intensity^2) +
-                              q_site2 * q_reg2 +
-                              mgm_interval * mgm_intensity,
-                            data = data_ST_LED)
-abs_model_info[[7]] <- glance(br_ST_LED_MP_abs) %>% 
-  mutate(model = "br_ST_LED_MP_abs",
-         simtype = "ST",
-         nat_haz = "LED",
-         profile = "MP")
-
-# ST LED IP
-br_ST_LED_IP_abs <- betareg(sha_i_IP_met_abs ~ stratum +
-                              q_site2 + q_reg2 +
-                              init +
-                              mgm_type +
-                              mgm_interval + mgm_intensity +
-                              I(mgm_interval^2) + I(mgm_intensity^2) +
-                              q_site2 * q_reg2 +
-                              mgm_interval * mgm_intensity,
-                            data = data_ST_LED)
-abs_model_info[[8]] <- glance(br_ST_LED_IP_abs) %>% 
-  mutate(model = "br_ST_LED_IP_abs",
-         simtype = "ST",
-         nat_haz = "LED",
-         profile = "IP")
+models <- vector(mode = "list", length = nrow(model_combinations))
+glances <- models
 
 
-# build models for diff values ----------------------------------------
-diff_model_info <- vector(mode = "list", length = 8)
+# build models ------------------------------------------------------------
+for (i in 1:nrow(model_combinations)) {
+  cat("\r ", i, "/", nrow(model_combinations))
+  vals <- model_combinations[i, ]
+  
+  # response variable
+  if (pull(vals, resp_var_type) == "abs") {
+    if (pull(vals, profile) == "MP") {
+      m_resp_var <- "sha_i_MP_met_abs"
+    } else {
+      m_resp_var <- "sha_i_IP_met_abs"
+    }
+  } else {
+    if (pull(vals, profile) == "MP") {
+      m_resp_var <- "sha_i_MP_met_diff_t"
+    } else {
+      m_resp_var <- "sha_i_IP_met_diff_t"
+    }
+  }
+  
+  # formula
+  if (pull(vals, simtype) == "LT") {
+    m_formula <- str_c(m_resp_var, " ~ ",
+                       "stratum + q_site2 * q_reg2 + mgm_type + mgm_interval + mgm_intensity")
+  } else {
+    m_formula <- str_c(m_resp_var, " ~ ",
+                       "stratum + q_site2 * q_reg2 + init +mgm_type + mgm_interval + mgm_intensity")
+  }
+  
+  if (pull(vals, formula_nr) == 2) {
+    m_formula <- str_c(m_formula, " + I(mgm_interval^2) + I(mgm_intensity^2)")
+  }
+  
+  if (pull(vals, formula_nr) == 3) {
+    m_formula <- str_c(m_formula, " + I(mgm_interval^2) + I(mgm_intensity^2) + mgm_interval * mgm_intensity")
+  }
+  
+  m_formula <- as.formula(m_formula)
+  
+  # data
+  m_data <- str_c("data_", pull(vals, simtype), "_", pull(vals, nat_haz))
+  
+  # link
+  m_link <- pull(vals, link)
+  
+  # build model
+  m <- eval(
+    bquote(betareg(formula = .(m_formula),
+                   data    = .(as.name(m_data)),
+                   link    = .(m_link)))
+  )
+  
+  # save model
+  models[[i]] <- m
+  
+  # save glance
+  glances[[i]] <- bind_cols(vals, glance(m))
+}
+cat("\n")
 
-## LT A MP
-br_LT_A_MP_diff <- betareg(sha_i_MP_met_diff_t ~ stratum +
-                             q_site2 + q_reg2 +
-                             mgm_type +
-                             mgm_interval + mgm_intensity +
-                             I(mgm_interval^2) + I(mgm_intensity^2) +
-                             q_site2 * q_reg2 +
-                             mgm_interval * mgm_intensity,
-                           data = data_LT_A)
-diff_model_info[[1]] <- glance(br_LT_A_MP_diff) %>% 
-  mutate(model = "br_LT_A_MP_diff",
-         simtype = "LT",
-         nat_haz = "A",
-         profile = "MP")
-
-# LT A IP
-br_LT_A_IP_diff <- betareg(sha_i_IP_met_diff_t ~ stratum +
-                             q_site2 + q_reg2 +
-                             mgm_type +
-                             mgm_interval + mgm_intensity +
-                             I(mgm_interval^2) + I(mgm_intensity^2) +
-                             q_site2 * q_reg2 +
-                             mgm_interval * mgm_intensity,
-                           data = data_LT_A)
-diff_model_info[[2]] <- glance(br_LT_A_IP_diff) %>% 
-  mutate(model = "br_LT_A_IP_diff",
-         simtype = "LT",
-         nat_haz = "A",
-         profile = "IP")
-
-# LT LED MP
-br_LT_LED_MP_diff <- betareg(sha_i_MP_met_diff_t ~ stratum +
-                               q_site2 + q_reg2 +
-                               mgm_type +
-                               mgm_interval + mgm_intensity +
-                               I(mgm_interval^2) + I(mgm_intensity^2) +
-                               q_site2 * q_reg2 +
-                               mgm_interval * mgm_intensity,
-                             data = data_LT_LED)
-diff_model_info[[3]] <- glance(br_LT_LED_MP_diff) %>% 
-  mutate(model = "br_LT_LED_MP_diff",
-         simtype = "LT",
-         nat_haz = "LED",
-         profile = "MP")
-
-# LT LED IP
-br_LT_LED_IP_diff <- betareg(sha_i_IP_met_diff_t ~ stratum +
-                               q_site2 + q_reg2 +
-                               mgm_type +
-                               mgm_interval + mgm_intensity +
-                               I(mgm_interval^2) + I(mgm_intensity^2) +
-                               q_site2 * q_reg2 +
-                               mgm_interval * mgm_intensity,
-                             data = data_LT_LED)
-diff_model_info[[4]] <- glance(br_LT_LED_IP_diff) %>% 
-  mutate(model = "br_LT_LED_IP_diff",
-         simtype = "LT",
-         nat_haz = "LED",
-         profile = "IP")
-
-## ST A MP
-br_ST_A_MP_diff <- betareg(sha_i_MP_met_diff_t ~ stratum +
-                             q_site2 + q_reg2 +
-                             init +
-                             mgm_type +
-                             mgm_interval + mgm_intensity +
-                             I(mgm_interval^2) + I(mgm_intensity^2) +
-                             q_site2 * q_reg2 +
-                             mgm_interval * mgm_intensity,
-                           data = data_ST_A)
-diff_model_info[[5]] <- glance(br_ST_A_MP_diff) %>% 
-  mutate(model = "br_ST_A_MP_diff",
-         simtype = "ST",
-         nat_haz = "A",
-         profile = "MP")
-
-# ST A IP
-br_ST_A_IP_diff <- betareg(sha_i_IP_met_diff_t ~ stratum +
-                             q_site2 + q_reg2 +
-                             init +
-                             mgm_type +
-                             mgm_interval + mgm_intensity +
-                             I(mgm_interval^2) + I(mgm_intensity^2) +
-                             q_site2 * q_reg2 +
-                             mgm_interval * mgm_intensity,
-                           data = data_ST_A)
-diff_model_info[[6]] <- glance(br_ST_A_IP_diff) %>% 
-  mutate(model = "br_ST_A_IP_diff",
-         simtype = "ST",
-         nat_haz = "A",
-         profile = "IP")
-
-# ST LED MP
-br_ST_LED_MP_diff <- betareg(sha_i_MP_met_diff_t ~ stratum +
-                               q_site2 + q_reg2 +
-                               init +
-                               mgm_type +
-                               mgm_interval + mgm_intensity +
-                               I(mgm_interval^2) + I(mgm_intensity^2) +
-                               q_site2 * q_reg2 +
-                               mgm_interval * mgm_intensity,
-                             data = data_ST_LED)
-diff_model_info[[7]] <- glance(br_ST_LED_MP_diff) %>% 
-  mutate(model = "br_ST_LED_MP_diff",
-         simtype = "ST",
-         nat_haz = "LED",
-         profile = "MP")
-
-# ST LED IP
-br_ST_LED_IP_diff <- betareg(sha_i_IP_met_diff_t ~ stratum +
-                               q_site2 + q_reg2 +
-                               init +
-                               mgm_type +
-                               mgm_interval + mgm_intensity +
-                               I(mgm_interval^2) + I(mgm_intensity^2) +
-                               q_site2 * q_reg2 +
-                               mgm_interval * mgm_intensity,
-                             data = data_ST_LED)
-diff_model_info[[8]] <- glance(br_ST_LED_IP_diff) %>% 
-  mutate(model = "br_ST_LED_IP_diff",
-         simtype = "ST",
-         nat_haz = "LED",
-         profile = "IP")
+glances <- bind_rows(glances)
 
 
-# overview ----------------------------------------------------------------
-abs_model_info <- bind_rows(abs_model_info) %>% 
-  mutate(resp_var = "abs") %>% 
-  select(model:profile, resp_var, pseudo.r.squared:nobs)
-
-diff_model_info <- bind_rows(diff_model_info) %>% 
-  mutate(resp_var = "diff") %>% 
-  select(model:profile, resp_var, pseudo.r.squared:nobs)
-
-all_model_info <- bind_rows(abs_model_info, diff_model_info)
+# compare models ----------------------------------------------------------
+glances
 
 
 # visualize effects -------------------------------------------------------
-model_names <- ls()[str_detect(ls(), "br_")]
-
-for (i in seq_along(model_names)) {
-  jpeg(filename = str_c(folder_out, model_names[i], "_1.jpg"),
-       width = 1000,
-       height = 700) 
-  plot(allEffects(mod = get(model_names[i])))
+for (i in seq_along(models)) {
+  cat("\r ", i, "/", length(models))
+  
+  vals <- model_combinations[i, ]
+  
+  jpeg(filename = str_c(folder_out, "y_auto/br_",
+                        pull(vals, simtype), "_",
+                        pull(vals, nat_haz), "_",
+                        pull(vals, profile), "_",
+                        pull(vals, resp_var_type), "_",
+                        "f", pull(vals, formula_nr), ".jpg"),
+       width    = 1000,
+       height   = 700)
+  plot(allEffects(mod = models[[i]]))
   dev.off()
   
-  jpeg(filename = str_c(folder_out, model_names[i], "_2.jpg"),
-       width = 1000,
-       height = 700) 
-  plot(allEffects(mod = get(model_names[i])),
+  jpeg(filename = str_c(folder_out, "y_01/br_",
+                        pull(vals, simtype), "_",
+                        pull(vals, nat_haz), "_",
+                        pull(vals, profile), "_",
+                        pull(vals, resp_var_type), "_",
+                        "f", pull(vals, formula_nr), ".jpg"),
+       width    = 1000,
+       height   = 700)
+  plot(allEffects(mod = models[[i]]),
        axes = list(y = list(lim = c(0, 1),
                             type = "response")))
   dev.off()
 }
-
-
-
