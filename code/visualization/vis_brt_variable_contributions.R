53c5ad444cd1d30879309737e32c1ef0b370f3dc
Ueli Schmid <ueli.schmid@usys.ethz.ch>
2023-02-24 13:22:19 +0100

    vis brt variable contributions: add graph for mgm-contribution

--- code/visualization/vis_brt_variable_contributions.R
+++ code/visualization/vis_brt_variable_contributions.R
@@ -23,7 +23,7 @@ plot_vi <- function(m_all, mc_all,
   mc_used <- mc_all %>% 
     filter(simtype == st) %>% 
     filter(resp_var_type == rv)
-
+  
   contributions <- vector(mode = "list", length = nrow(mc_used))
   
   for (i in 1:nrow(mc_used)) {
@@ -67,7 +67,72 @@ plot_vi <- function(m_all, mc_all,
     scale_fill_discrete(guide = guide_legend(reverse = TRUE)) +
     labs(title = title_str,
          x     = NULL,
-         y     = "Variable importance (%)") +
+         y     = "Variable importance (%)",
+         fill  = "Profile") +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank())
+  
+  return(gg_p)
+}
+
+plot_mgmi <- function(m_all, mc_all,
+                      st, rv) {
+  mc_used <- mc_all %>% 
+    filter(simtype == st) %>% 
+    filter(resp_var_type == rv)
+  
+  contributions <- vector(mode = "list", length = nrow(mc_used))
+  
+  for (i in 1:nrow(mc_used)) {
+    vals <- mc_used[i, ]
+    
+    contributions[[i]] <- as_tibble(m_all[[pull(vals, rowid)]]$contributions) %>% 
+      mutate(simtype       = pull(vals, simtype),
+             nat_haz       = pull(vals, nat_haz),
+             profile       = pull(vals, profile),
+             resp_var_type = pull(vals, resp_var_type))
+  }
+  
+  contributions <- bind_rows(contributions) %>% 
+    mutate(label = str_c(nat_haz, " - ", profile),
+           var   = str_replace_all(var,
+                                   c("stratum"       = "Stratum",
+                                     "q_reg2"        = "Qreg",
+                                     "q_site2"       = "Qsite",
+                                     "mgm_type"      = "Type",
+                                     "mgm_interval"  = "Interval",
+                                     "mgm_intensity" = "Intensity",
+                                     "init"          = "Init")))
+  
+  var_order <- contributions %>% 
+    group_by(var) %>% 
+    summarise(m_inf = mean(rel.inf)) %>% 
+    arrange(desc(m_inf)) %>% 
+    pull(var)
+  
+  contributions <- contributions %>% 
+    mutate(var   = factor(var, levels = var_order),
+           var   = fct_rev(var),
+           label = factor(label, levels = c("A - MP", "A - IP", "LED - MP", "LED - IP")),
+           label = fct_rev(label))
+  
+  mgm_contribution <- contributions %>% 
+    mutate(mgm_var = case_when(var %in% c("Type", "Interval", "Intensity") ~ "mgm",
+                               TRUE                                        ~ "other")) %>% 
+    group_by(label, mgm_var) %>% 
+    summarise(rel.inf.sum = sum(rel.inf),
+              .groups     = "drop")
+  
+  title_str <- str_c(st, " - ", rv)
+  
+  gg_p <- mgm_contribution %>% 
+    filter(mgm_var == "mgm") %>% 
+    ggplot(aes(label, rel.inf.sum)) +
+    geom_col() +
+    coord_flip() +
+    labs(title = title_str,
+         x     = NULL,
+         y     = "Importance of management (%)") +
     theme_bw() +
     theme(panel.grid.minor = element_blank())
   
@@ -81,25 +146,49 @@ plot_vi(m_all  = models_full,
         mc_all = model_combinations,
         st     = "LT",
         rv     = "abs")
-ggsave(str_c(folder_out, "LT_abs_0full.png"))
+ggsave(str_c(folder_out, "LT_abs_full.png"))
+
+plot_mgmi(m_all  = models_full,
+          mc_all = model_combinations,
+          st     = "LT",
+          rv     = "abs")
+ggsave(str_c(folder_out, "LT_abs_full_mgm.png"))
 
 # LT diff
 plot_vi(m_all  = models_full,
         mc_all = model_combinations,
         st     = "LT",
         rv     = "diff")
-ggsave(str_c(folder_out, "LT_diff_0full.png"))
+ggsave(str_c(folder_out, "LT_diff_full.png"))
+
+plot_mgmi(m_all  = models_full,
+          mc_all = model_combinations,
+          st     = "LT",
+          rv     = "diff")
+ggsave(str_c(folder_out, "LT_diff_full_mgm.png"))
 
 # ST abs
 plot_vi(m_all  = models_full,
         mc_all = model_combinations,
         st     = "ST",
         rv     = "abs")
-ggsave(str_c(folder_out, "ST_abs_0full.png"))
+ggsave(str_c(folder_out, "ST_abs_full.png"))
+
+plot_mgmi(m_all  = models_full,
+          mc_all = model_combinations,
+          st     = "ST",
+          rv     = "abs")
+ggsave(str_c(folder_out, "ST_abs_full_mgm.png"))
 
 # ST diff
 plot_vi(m_all  = models_full,
         mc_all = model_combinations,
         st     = "ST",
         rv     = "diff")
-ggsave(str_c(folder_out, "ST_diff_0full.png"))
+ggsave(str_c(folder_out, "ST_diff_full.png"))
+
+plot_mgmi(m_all  = models_full,
+          mc_all = model_combinations,
+          st     = "ST",
+          rv     = "diff")
+ggsave(str_c(folder_out, "ST_diff_full_mgm.png"))
