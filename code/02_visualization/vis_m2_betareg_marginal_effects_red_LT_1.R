5ca66540389b8f9372f865eb9bdd22d4e37db08b
Ueli Schmid <ueli.schmid@usys.ethz.ch>
2023-02-28 11:43:32 +0100

    vis betareg v2 red marginal effects: add title and move output folder

--- code/02_visualization/vis_m2_betareg_marginal_effects_red_LT_1.R
+++ code/02_visualization/vis_m2_betareg_marginal_effects_red_LT_1.R
@@ -1,6 +1,7 @@
 ## visualize marginal effects of betareg-models
-## v2 red ST
-## 24.2.23, us
+## v2 red LT
+## variant 1 (all predictors)
+## 28.2.23, us
 
 
 # setup -------------------------------------------------------------------
@@ -10,29 +11,27 @@ library(margins)
 
 folder_in_d <- "data/processed/nais_analysis_data/"
 folder_in_m <- "data/processed/betareg_models_v2/"
-folder_out <- "results/vis_models_v2/betareg/vis_betareg_red_marginal_effects/"
+folder_out <- "results/vis_models_v2/betareg/vis_betareg_red_marginal_effects_1/"
 
 
 # load data ---------------------------------------------------------------
 data_analysis <- read_rds(str_c(folder_in_d, "analysis_data_transf.rds")) %>% 
   filter(q_reg2 == "normal")
 
-data_comb <- expand_grid(simtype = "ST",
+# subset data and save as new objects
+data_comb <- expand_grid(simtype = "LT",
                          nat_haz = c("A", "LED"),
-                         stratum = c("UM", "HM", "SA"),
-                         init    = c("1", "2", "3"))
+                         stratum = c("UM", "HM", "SA"))
 for (i in 1:nrow(data_comb)) {
   vals <- data_comb[i,]
   object_name <- str_c("data_",
                        pull(vals, simtype), "_",
                        pull(vals, nat_haz), "_",
-                       pull(vals, stratum), "_",
-                       pull(vals, init))
+                       pull(vals, stratum))
   object_content <- data_analysis %>% 
     filter(simtype == pull(vals, simtype)) %>% 
     filter(nat_haz == pull(vals, nat_haz)) %>% 
-    filter(stratum == pull(vals, stratum)) %>% 
-    filter(init    == pull(vals, init))
+    filter(stratum == pull(vals, stratum))
   
   assign(object_name, object_content)
 }
@@ -41,8 +40,8 @@ rm(object_name, object_content)
 
 
 # load models -------------------------------------------------------------
-models <- read_rds(str_c(folder_in_m, "models_red_ST.rds"))
-model_combinations <- read_rds(str_c(folder_in_m, "model_combinations_red_ST.rds")) %>% 
+models <- read_rds(str_c(folder_in_m, "models_red_LT.rds"))
+model_combinations <- read_rds(str_c(folder_in_m, "model_combinations_red_LT.rds")) %>% 
   rowid_to_column()
 
 
@@ -61,36 +60,33 @@ plot_mageff <- function(m_all, mc_all, rv) {
       as_tibble() %>% 
       mutate(nat_haz = pull(vals, nat_haz),
              profile = pull(vals, profile),
-             stratum = pull(vals, stratum),
-             init    = pull(vals, init))
+             stratum = pull(vals, stratum))
   }
   
   margs <- bind_rows(margs) %>% 
-    mutate(Predictor = factor,
-           Predictor = str_replace_all(Predictor,
-                                       c("stratumHM"      = "Stratum: HM",
-                                         "stratumSA"      = "Stratum: SA",
-                                         "q_site2medium"  = "Qsite: medium",
-                                         "q_reg2hindered" = "Qreg: hindered",
-                                         "mgm_typeSC"     = "Type: SC",
-                                         "mgm_typeGRS2"   = "Type: GRS2",
-                                         "mgm_typeGRS1"   = "Type: GRS1",
-                                         "mgm_typeCAB2"   = "Type: CAB2",
-                                         "mgm_typeCAB1"   = "Type: CAB1",
-                                         "mgm_interval"   = "Interval",
-                                         "mgm_intensity"  = "Intensity",
-                                         "init2"          = "Init: 2",
-                                         "init3"          = "Init: 3")),
-           Predictor = factor(Predictor, levels = c("Stratum: HM", "Stratum: SA",
-                                                    "Init: 2", "Init: 3",
-                                                    "Qsite: medium", "Qreg: hindered",
-                                                    "Type: GRS1", "Type: GRS2", "Type: CAB1", "Type: CAB2", "Type: SC",
-                                                    "Interval", "Intensity")),
-           Predictor = fct_rev(Predictor),
-           profile   = factor(profile, levels = c("MP", "IP")),
-           stratum   = factor(stratum, levels = c("UM", "HM", "SA"))) %>% 
-    rename(Stratum = stratum,
-           Init    = init)
+  mutate(Predictor = factor,
+         Predictor = str_replace_all(Predictor,
+                                     c("stratumHM"      = "Stratum: HM",
+                                       "stratumSA"      = "Stratum: SA",
+                                       "q_site2medium"  = "Qsite: medium",
+                                       "q_reg2hindered" = "Qreg: hindered",
+                                       "mgm_typeSC"     = "Type: SC",
+                                       "mgm_typeGRS2"   = "Type: GRS2",
+                                       "mgm_typeGRS1"   = "Type: GRS1",
+                                       "mgm_typeCAB2"   = "Type: CAB2",
+                                       "mgm_typeCAB1"   = "Type: CAB1",
+                                       "mgm_interval"   = "Interval",
+                                       "mgm_intensity"  = "Intensity",
+                                       "init2"          = "Init: 2",
+                                       "init3"          = "Init: 3")),
+         Predictor = factor(Predictor, levels = c("Stratum: HM", "Stratum: SA",
+                                                  "Init: 2", "Init: 3",
+                                                  "Qsite: medium", "Qreg: hindered",
+                                                  "Type: GRS1", "Type: GRS2", "Type: CAB1", "Type: CAB2", "Type: SC",
+                                                  "Interval", "Intensity")),
+         Predictor = fct_rev(Predictor),
+         profile   = factor(profile, levels = c("MP", "IP")),
+         stratum   = factor(stratum, levels = c("UM", "HM", "SA")))
   
   gg_p <- ggplot(margs, aes(Predictor, AME,
                             color    = nat_haz,
@@ -104,14 +100,13 @@ plot_mageff <- function(m_all, mc_all, rv) {
     geom_hline(yintercept = 0) +
     geom_vline(xintercept = c(1.5, 2.5, 7.5),
                linetype   = 2) +
-    labs(y        = "Average marginal effect",
+    labs(title    = str_c("LT ", rv),
+         y        = "Average marginal effect",
          color    = "Natural\nhazard",
          shape    = "Profile",
          linetype = "Profile") +
     coord_flip() +
-    facet_grid(rows     = vars(Init),
-               cols     = vars(Stratum),
-               labeller = "label_both") +
+    facet_wrap(~stratum) +
     theme_bw() +
     theme(panel.grid.minor = element_blank())
   
@@ -125,10 +120,10 @@ p_abs <- plot_mageff(m_all  = models,
                      mc_all = model_combinations,
                      rv     = "abs")
 
-ggsave(filename = str_c(folder_out, "ST_abs.jpg"),
+ggsave(filename = str_c(folder_out, "LT_abs.jpg"),
        plot     = p_abs,
        width    = 25,
-       height   = 25,
+       height   = 15,
        units    = "cm",
        scale    = 1)
 
@@ -138,9 +133,9 @@ p_diff <- plot_mageff(m_all  = models,
                       mc_all = model_combinations,
                       rv     = "diff")
 
-ggsave(filename = str_c(folder_out, "ST_diff.jpg"),
+ggsave(filename = str_c(folder_out, "LT_diff.jpg"),
        plot     = p_diff,
        width    = 25,
-       height   = 25,
+       height   = 15,
        units    = "cm",
        scale    = 1)
--- code/02_visualization/vis_m2_betareg_marginal_effects_red_ST_1.R
+++ code/02_visualization/vis_m2_betareg_marginal_effects_red_ST_1.R
@@ -1,6 +1,7 @@
 ## visualize marginal effects of betareg-models
-## v2 red LT
-## 24.2.23, us
+## v2 red ST
+## variant 1 (all predictors)
+## 28.2.23, us
 
 
 # setup -------------------------------------------------------------------
@@ -10,27 +11,29 @@ library(margins)
 
 folder_in_d <- "data/processed/nais_analysis_data/"
 folder_in_m <- "data/processed/betareg_models_v2/"
-folder_out <- "results/vis_models_v2/betareg/vis_betareg_red_marginal_effects/"
+folder_out <- "results/vis_models_v2/betareg/vis_betareg_red_marginal_effects_1/"
 
 
 # load data ---------------------------------------------------------------
 data_analysis <- read_rds(str_c(folder_in_d, "analysis_data_transf.rds")) %>% 
   filter(q_reg2 == "normal")
 
-# subset data and save as new objects
-data_comb <- expand_grid(simtype = "LT",
+data_comb <- expand_grid(simtype = "ST",
                          nat_haz = c("A", "LED"),
-                         stratum = c("UM", "HM", "SA"))
+                         stratum = c("UM", "HM", "SA"),
+                         init    = c("1", "2", "3"))
 for (i in 1:nrow(data_comb)) {
   vals <- data_comb[i,]
   object_name <- str_c("data_",
                        pull(vals, simtype), "_",
                        pull(vals, nat_haz), "_",
-                       pull(vals, stratum))
+                       pull(vals, stratum), "_",
+                       pull(vals, init))
   object_content <- data_analysis %>% 
     filter(simtype == pull(vals, simtype)) %>% 
     filter(nat_haz == pull(vals, nat_haz)) %>% 
-    filter(stratum == pull(vals, stratum))
+    filter(stratum == pull(vals, stratum)) %>% 
+    filter(init    == pull(vals, init))
   
   assign(object_name, object_content)
 }
@@ -39,8 +42,8 @@ rm(object_name, object_content)
 
 
 # load models -------------------------------------------------------------
-models <- read_rds(str_c(folder_in_m, "models_red_LT.rds"))
-model_combinations <- read_rds(str_c(folder_in_m, "model_combinations_red_LT.rds")) %>% 
+models <- read_rds(str_c(folder_in_m, "models_red_ST.rds"))
+model_combinations <- read_rds(str_c(folder_in_m, "model_combinations_red_ST.rds")) %>% 
   rowid_to_column()
 
 
@@ -59,33 +62,36 @@ plot_mageff <- function(m_all, mc_all, rv) {
       as_tibble() %>% 
       mutate(nat_haz = pull(vals, nat_haz),
              profile = pull(vals, profile),
-             stratum = pull(vals, stratum))
+             stratum = pull(vals, stratum),
+             init    = pull(vals, init))
   }
   
   margs <- bind_rows(margs) %>% 
-  mutate(Predictor = factor,
-         Predictor = str_replace_all(Predictor,
-                                     c("stratumHM"      = "Stratum: HM",
-                                       "stratumSA"      = "Stratum: SA",
-                                       "q_site2medium"  = "Qsite: medium",
-                                       "q_reg2hindered" = "Qreg: hindered",
-                                       "mgm_typeSC"     = "Type: SC",
-                                       "mgm_typeGRS2"   = "Type: GRS2",
-                                       "mgm_typeGRS1"   = "Type: GRS1",
-                                       "mgm_typeCAB2"   = "Type: CAB2",
-                                       "mgm_typeCAB1"   = "Type: CAB1",
-                                       "mgm_interval"   = "Interval",
-                                       "mgm_intensity"  = "Intensity",
-                                       "init2"          = "Init: 2",
-                                       "init3"          = "Init: 3")),
-         Predictor = factor(Predictor, levels = c("Stratum: HM", "Stratum: SA",
-                                                  "Init: 2", "Init: 3",
-                                                  "Qsite: medium", "Qreg: hindered",
-                                                  "Type: GRS1", "Type: GRS2", "Type: CAB1", "Type: CAB2", "Type: SC",
-                                                  "Interval", "Intensity")),
-         Predictor = fct_rev(Predictor),
-         profile   = factor(profile, levels = c("MP", "IP")),
-         stratum   = factor(stratum, levels = c("UM", "HM", "SA")))
+    mutate(Predictor = factor,
+           Predictor = str_replace_all(Predictor,
+                                       c("stratumHM"      = "Stratum: HM",
+                                         "stratumSA"      = "Stratum: SA",
+                                         "q_site2medium"  = "Qsite: medium",
+                                         "q_reg2hindered" = "Qreg: hindered",
+                                         "mgm_typeSC"     = "Type: SC",
+                                         "mgm_typeGRS2"   = "Type: GRS2",
+                                         "mgm_typeGRS1"   = "Type: GRS1",
+                                         "mgm_typeCAB2"   = "Type: CAB2",
+                                         "mgm_typeCAB1"   = "Type: CAB1",
+                                         "mgm_interval"   = "Interval",
+                                         "mgm_intensity"  = "Intensity",
+                                         "init2"          = "Init: 2",
+                                         "init3"          = "Init: 3")),
+           Predictor = factor(Predictor, levels = c("Stratum: HM", "Stratum: SA",
+                                                    "Init: 2", "Init: 3",
+                                                    "Qsite: medium", "Qreg: hindered",
+                                                    "Type: GRS1", "Type: GRS2", "Type: CAB1", "Type: CAB2", "Type: SC",
+                                                    "Interval", "Intensity")),
+           Predictor = fct_rev(Predictor),
+           profile   = factor(profile, levels = c("MP", "IP")),
+           stratum   = factor(stratum, levels = c("UM", "HM", "SA"))) %>% 
+    rename(Stratum = stratum,
+           Init    = init)
   
   gg_p <- ggplot(margs, aes(Predictor, AME,
                             color    = nat_haz,
@@ -99,12 +105,15 @@ plot_mageff <- function(m_all, mc_all, rv) {
     geom_hline(yintercept = 0) +
     geom_vline(xintercept = c(1.5, 2.5, 7.5),
                linetype   = 2) +
-    labs(y        = "Average marginal effect",
+    labs(title    = str_c("ST ", rv),
+         y        = "Average marginal effect",
          color    = "Natural\nhazard",
          shape    = "Profile",
          linetype = "Profile") +
     coord_flip() +
-    facet_wrap(~stratum) +
+    facet_grid(rows     = vars(Init),
+               cols     = vars(Stratum),
+               labeller = "label_both") +
     theme_bw() +
     theme(panel.grid.minor = element_blank())
   
@@ -118,10 +127,10 @@ p_abs <- plot_mageff(m_all  = models,
                      mc_all = model_combinations,
                      rv     = "abs")
 
-ggsave(filename = str_c(folder_out, "LT_abs.jpg"),
+ggsave(filename = str_c(folder_out, "ST_abs.jpg"),
        plot     = p_abs,
        width    = 25,
-       height   = 15,
+       height   = 25,
        units    = "cm",
        scale    = 1)
 
@@ -131,9 +140,9 @@ p_diff <- plot_mageff(m_all  = models,
                       mc_all = model_combinations,
                       rv     = "diff")
 
-ggsave(filename = str_c(folder_out, "LT_diff.jpg"),
+ggsave(filename = str_c(folder_out, "ST_diff.jpg"),
        plot     = p_diff,
        width    = 25,
-       height   = 15,
+       height   = 25,
        units    = "cm",
        scale    = 1)
